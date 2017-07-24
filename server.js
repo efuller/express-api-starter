ce6d306f941a098b79da09590abcb97d26e19f3e
Eric Fuller <info@glassmoonmedia.com>
2017-07-24 07:10:59 -0400

    Other basic changes to get Mongo configured as well as get basic JWT Authentication up and running!

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./api/api');
 
 // The app.
 const app = express();
 
+// Setting up basic middleware for all Express requests
+app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
+app.use(bodyParser.json()); // Send JSON responses
+app.use(cors());
+
 // Setup a generic route.
-app.use('/', (req, res) => {
-	res.json('Here is the API Server!');
-});
+router(app);
 
 module.exports = app;
