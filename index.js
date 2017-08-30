121d153d028d59b2b7fcb96679d586f5da7b2646
Eric Fuller <info@glassmoonmedia.com>
2017-08-30 07:43:01 -0400

    Chores.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
 if (process.env.NODE_ENV !== 'production') {
 	// Environment Variables
+	const dotenv = require('dotenv');
 	dotenv.config();
 }
 
